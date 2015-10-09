99ef783baf40cb2606c4c378ad27b2a73afd31d6
michelbueno <bueno.michel@gmail.com>
2015-10-09 14:25:30 -0300

    Configura workspace

---
+++ Cafezin/CoffeMakerRepository.swift
@@ -0,0 +1,10 @@
+//
+//  CoffeMakerRepository.swift
+//  Cafezin
+//
+//  Created by mbueno on 10/9/15.
+//  Copyright © 2015 Thoughtworks. All rights reserved.
+//
+
+import Foundation
+import Alamofire
